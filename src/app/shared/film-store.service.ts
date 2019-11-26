0e7c5509ab578bf5b31f349ba1f7a4e4af851c83
Florian Will <osx.will1@gmail.com>
2019-11-26 18:20:42 +0100

    Fehlerfrei

--- src/app/shared/film-store.service.ts
+++ src/app/shared/film-store.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Film } from './film';
+import firestoreParser from 'firestore-parser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmStoreService {
-  private url = 'https://firestore.googleapis.com/v1/projects/filmlibrary-167373/databases/(default)/documents';
+  private url =
+    'https://firestore.googleapis.com/v1/projects/filmlibrary-167373/databases/(default)/documents';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  parseFirestoreEntry(firestoreEntry: any): Film {
+    return firestoreParser(firestoreEntry['fields']);
+  }
 
   getAll(): Observable<Film[]> {
-    return this.http.get<any[]>(`${this.url}/films`);
+    return this.http.get<object>(`${this.url}/films`).pipe(
+      map((resultJson): any[] => resultJson['documents']),
+      map(resultArray =>
+        resultArray.map(arrayEntry => this.parseFirestoreEntry(arrayEntry))
+      )
+    );
   }
 
   getSingle(gtin: string): Observable<Film> {
-    return this.http.get<any>(`${this.url}/films/${gtin}`);
+    return this.http.get<any>(`${this.url}/films/${gtin}`).pipe(
+      map(singleEntry => this.parseFirestoreEntry(singleEntry))
+    );
   }
 }
